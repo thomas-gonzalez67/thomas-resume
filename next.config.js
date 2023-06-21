714cea87065361461b7c3961938af969d3e8c5f8
thomas gonzalez <thomasgonz67@gmail.com>
2023-06-21 00:12:20 -0400

    fixing cors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    async rewrites() {
+    async headers() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'https://thomas-res-wordpress-d1dd47.ingress-earth.ewp.live/**',
-            },
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
         ]
     },
     webpack: (config, options) =>
